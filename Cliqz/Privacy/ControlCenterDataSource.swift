afd4ab760a4a1ac0d87760dd8111919d2c3fa503
Tim Palade <timotei@cliqz.com>
2018-04-27 11:10:47 +0200

    Extend DataSource

--- Cliqz/Privacy/ControlCenterDataSource.swift
+++ Cliqz/Privacy/ControlCenterDataSource.swift
@@ -25,10 +25,134 @@ protocol ControlCenterDSProtocol: class {
 
 class ControlCenterDataSource: ControlCenterDSProtocol {
     
-    let domainStr: String
+    let allCategories = ["advertising": "Advertising",
+                         "audio_video_player": "Audio/Video Player",
+                         "comments": "Comments",
+                         "customer_interaction": "Customer Interaction",
+                         "essential": "Essential",
+                         "pornvertising": "Adult Advertising",
+                         "site_analytics": "Site Analytics",
+                         "social_media": "Social Media",
+                         "uncategorized": "Uncategorized"
+    ]
     
+    let categories = ["advertising", "audio_video_player", "comments", "essential", "pornvertising", "site_analytics", "social_media", "uncategorized"]
+    
+    enum TableType {
+        case page
+        case global
+    }
+    
+    let domainStr: String
+    let pageTrackers: Dictionary<String, [TrackerListApp]>
+    let globalTrackers: Dictionary<String, [TrackerListApp]>
+    
+    //TODO: update mechanism
     init(url: URL) {
         self.domainStr = url.normalizedHost ?? url.absoluteString
+        self.pageTrackers = TrackerList.instance.trackersByCategory(for: self.domainStr)
+        self.globalTrackers = TrackerList.instance.trackersByCategory()
+    }
+    
+    func source(_ tableType: TableType) -> Dictionary<String, [TrackerListApp]> {
+        if tableType == .page {
+            return self.pageTrackers
+        }
+        
+        return self.globalTrackers
+    }
+    
+    func trackers(tableType: TableType, category: String) -> [TrackerListApp] {
+        return source(tableType)[category] ?? []
+    }
+    
+    func tracker(tableType: TableType, indexPath: IndexPath) -> TrackerListApp? {
+        let (section, row) = sectionAndRow(indexPath: indexPath)
+        let t = trackers(tableType: tableType, category: category(section))
+        guard t.isIndexValid(index: row) else { return nil }
+        return t[row]
+    }
+    
+    func category(_ section: Int) -> String {
+        guard categories.isIndexValid(index: section) else { return "" }
+        return categories[section]
+    }
+    
+    func sectionAndRow(indexPath: IndexPath) -> (Int, Int) {
+        return (indexPath.section, indexPath.row)
+    }
+    
+    //Section
+    func numberOfSections(tableType: TableType) -> Int {
+        return source(tableType).keys.count
+    }
+    
+    func numberOfRows(tableType: TableType, section: Int) -> Int {
+        return trackers(tableType: tableType, category: category(section)).count
+    }
+    
+    func title(tableType: TableType, section: Int) -> String {
+        return allCategories[category(section)] ?? ""
+    }
+    
+    func image(tableType: TableType, section: Int) -> UIImage? {
+        return UIImage(named: category(section))
+    }
+ 
+    func trackerCount(tableType: TableType, section: Int) -> Int {
+        return self.numberOfRows(tableType: tableType, section: section)
+    }
+    
+    func blockedTrackerCount(tableType: TableType, section: Int) -> Int {
+        return trackers(tableType: tableType, category: category(section)).filter({ (app) -> Bool in
+            let translatedState = app.state.translatedState
+            return translatedState == .blocked || translatedState == .restricted
+        }).count
+    }
+    
+    //Trackers - name, state
+    func title(tableType: TableType, indexPath: IndexPath) -> String? {
+        guard let t = tracker(tableType: tableType, indexPath: indexPath) else { return nil }
+        let state: TrackerStateEnum = t.state.translatedState
+        
+        if state == .blocked || state == .restricted {
+            return nil
+        }
+        
+        return t.name
+    }
+    
+    func attributedTitle(tableType: TableType, indexPath: IndexPath) -> NSMutableAttributedString? {
+        guard let t = tracker(tableType: tableType, indexPath: indexPath) else { return nil }
+        let state: TrackerStateEnum = t.state.translatedState
+        
+        if state == .blocked || state == .restricted {
+            let str = NSMutableAttributedString(string: t.name)
+            str.addAttributes([NSStrikethroughStyleAttributeName : 1], range: NSMakeRange(0, t.name.count))
+            return str
+        }
+        
+        return nil
+    }
+    
+    func stateIcon(tableType: TableType, indexPath: IndexPath) -> UIImage? {
+        guard let t = tracker(tableType: tableType, indexPath: indexPath) else { return nil }
+        
+        let domainState = self.domainState()
+        
+        if domainState == .restricted {
+            return UIImage(named: iconForTrackerState(state: .restricted))
+        }
+        else if domainState == .trusted {
+            return UIImage(named: iconForTrackerState(state: .trusted))
+        }
+        
+        return UIImage(named: iconForTrackerState(state: t.state.translatedState))
+    }
+    
+    func appId(tableType: TableType, indexPath: IndexPath) -> Int {
+        guard let t = tracker(tableType: tableType, indexPath: indexPath) else { return -1 }
+        return t.appId
     }
     
     func trackersByCategory() -> Dictionary<String, [TrackerListApp]> {
@@ -94,5 +218,27 @@ class ControlCenterDataSource: ControlCenterDSProtocol {
     
     func adblockCount() -> Int {
         return 0 //placeholder
+    }
+    
+    private func iconForTrackerState(state: TrackerStateEnum?) -> String {
+        if let state = state {
+            switch state {
+            case .none:
+                return ""
+            case .blocked:
+                return "blockTracker"
+            case .restricted:
+                return "restrictTracker"
+            case .trusted:
+                return "trustTracker"
+            }
+        }
+        return ""
+    }
+}
+
+extension Array {
+    func isIndexValid(index: Int) -> Bool {
+        return index >= 0 && index < self.count
     }
 }
